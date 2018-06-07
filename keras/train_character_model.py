70c488911f28db906e62c916caf3a565d73912ce
Matt Mistele <mmistele@stanford.edu>
2018-06-07 12:17:32 -0700

    Simplifying character model & using masking
    Split into two versions with different masking strategies

--- keras/train_character_model.py
+++ keras/train_character_model.py
@@ -11,17 +11,17 @@ from keras.initializers import glorot_uniform
 from keras.utils import to_categorical
 
 from embedding import strings_to_character_vecs
-from models import Character_Model
+from models import Character_Model_1
 from utils import read_csv, get_char_counts_from_csv, index_to_one_hot
 
 
-X_train, Y_train = read_csv('data/train-debug.csv') 
-X_test, Y_test = read_csv('data/test-debug.csv') 
+X_train, Y_train = read_csv('data/train.csv') 
+X_test, Y_test = read_csv('data/test.csv') 
 
 # maxLen = max(len(max(X_train, key=len)), len(max(X_test, key=len)))
 maxLen = len(max(X_train, key=len))
 
-counts = get_char_counts_from_csv('data/train-debug.csv') + get_char_counts_from_csv('data/test-debug.csv') 
+counts = get_char_counts_from_csv('data/train.csv') + get_char_counts_from_csv('data/test.csv') 
 most_common = counts.most_common()
 char_to_index = {x:i for i, (x, _) in enumerate(most_common)}
 index_to_char = {i:x for i, (x, _) in enumerate(most_common)}
@@ -32,7 +32,7 @@ char_to_vec_map = {x : index_to_one_hot(i, alphabet_size) for i, (x, _) in enume
 X_train_indices = strings_to_character_vecs(X_train, char_to_index, maxLen, alphabet_size)
 print(X_train_indices.shape)
 
-model = Character_Model((X_train_indices.shape[1], X_train_indices.shape[2]))
+model = Character_Model_1((X_train_indices.shape[1], X_train_indices.shape[2]))
 
 optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, decay=0.0, epsilon=None)
 model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
